561b25401ace32f75ceac77e1cb008302813b09a
Lauren Kashan <lkashan@gmail.com>
2020-11-09 15:07:23 -0500

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.new(
+  first_name: "Lauren",
+  last_name: "Kashan",
+  email: "lkashan@gmail.com",
+  phone_number: "410-404-9044",
+  donation_pledge: "3",
+  charity_1: "Trans Women Of Color Collective",
+  charity_1_link: "https://www.twocc.us"
+)
+user1.password = "123456"
+user1.save
+
+user2 = User.new(
+  first_name: "Vinnie",
+  last_name: "Lizard",
+  email: "MUSTCHOMPYELLOW@gmail.com",
+  phone_number: "666-666-6666",
+  donation_pledge: "10",
+  charity_1: "MAPS: Multidisciplinary Association for Psychadelic Studies",
+  charity_1_link: "https://maps.org/"
+)
+user2.password = "123456"
+user2.save
+
+user3 = User.new(
+  first_name: "Justin",
+  last_name: "Looper",
+  email: "jloops@gmail.com",
+  phone_number: "410-867-5309",
+  donation_pledge: "4",
+  charity_1: "Anti Police Terror Project",
+  charity_1_link: "https://www.antipoliceterrorproject.org/"
+)
+user3.password = "123456"
+user3.save
+
+user4 = User.new(
+  first_name: "Shakira",
+  last_name: "JumpingSpider",
+  email: "hipsdontlie@gmail.com",
+  phone_number: "123-456-7890",
+  donation_pledge: "15",
+  charity_1: "A Voice For The Innocent",
+  charity_1_link: "https://www.avoicefortheinnocent.org/"
+)
+user4.password = "123456"
+user4.save
