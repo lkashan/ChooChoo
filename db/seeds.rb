ece7ec054fe552e0ed506ef28e57c11822ac4115
Lauren Kashan <lkashan@gmail.com>
2021-01-07 22:13:14 -0500

    Updated Seeds w/ Charity Info

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ user1 = User.new(
   phone_number: "410-404-9044",
   donation_pledge: "3",
   charity_1: "Trans Women Of Color Collective",
+  charity_1_info: "Trans Women of Color Collective (TWOCC) was created to cultivate economic opportunities and affirming spaces for trans people of color and our families, to foster kinship, build community, engage in healing and restorative justice through arts, culture, media, advocacy and activism. Our efforts center those in our community who have not had access to resources, opportunities and sustainable systems of support to live unapologetically in their truths; Trans and gender non-conforming people of color, in particularly Black trans women and femmes, poor, indigenous, disabled, our seniors and youth who are disproportionately impacted by structural and state sanctioned violence that impacts our socio-economic growth and development and is inextricably linked to physical violence and discrimination we face daily.",
   charity_1_link: "https://www.twocc.us"
 )
 user1.password = "123456"
@@ -19,11 +20,12 @@ user1.save
 
 user2 = User.new(
   first_name: "Vinnie",
-  last_name: "Lizard",
+  last_name: "Varanid",
   email: "MUSTCHOMPYELLOW@gmail.com",
   phone_number: "666-666-6666",
   donation_pledge: "10",
   charity_1: "MAPS: Multidisciplinary Association for Psychadelic Studies",
+  charity_1_info: "Founded in 1986, the Multidisciplinary Association for Psychedelic Studies (MAPS) is a 501(c)(3) non-profit research and educational organization that develops medical, legal, and cultural contexts for people to benefit from the careful uses of psychedelics and marijuana.",
   charity_1_link: "https://maps.org/"
 )
 user2.password = "123456"
@@ -36,6 +38,7 @@ user3 = User.new(
   phone_number: "410-867-5309",
   donation_pledge: "4",
   charity_1: "Anti Police Terror Project",
+  charity_1_info: "The Anti Police-Terror Project is a Black-led, multi-racial, intergenerational coalition that seeks to build a replicable and sustainable model to eradicate police terror in communities of color. We support families surviving police terror in their fight for justice, documenting police abuses and connecting impacted families and community members with resources, legal referrals, and opportunities for healing.",
   charity_1_link: "https://www.antipoliceterrorproject.org/"
 )
 user3.password = "123456"
@@ -43,11 +46,12 @@ user3.save
 
 user4 = User.new(
   first_name: "Shakira",
-  last_name: "JumpingSpider",
+  last_name: "Salticidae",
   email: "hipsdontlie@gmail.com",
   phone_number: "123-456-7890",
   donation_pledge: "15",
   charity_1: "A Voice For The Innocent",
+  charity_1_info: "A Voice for the Innocent is a non-profit community dedicated to the support of those affected by sexual violence by offering a safe and compassionate space to share stories and seek help.",
   charity_1_link: "https://www.avoicefortheinnocent.org/"
 )
 user4.password = "123456"
